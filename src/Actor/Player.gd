4c5781fb9a6ebb12bc1d88e873d6d26eb0c2f4d3
Arvi-beep-boop <79840246+Arvi-beep-boop@users.noreply.github.com>
2021-03-05 19:50:45 +0100

    lil bit of new animations

--- src/Actor/Player.gd
+++ src/Actor/Player.gd
@@ -1,16 +1,21 @@
 extends Actor
 
+onready var animationPlayer = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
 	var direction: = get_direction()
 	velocity = calculate_move_velocity(velocity, direction, speed)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
-
+	
+	
+	
 func get_direction() -> Vector2:
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
 	)
+	
+	
 func calculate_move_velocity(
 	linear_velocity: Vector2,
 	direction: Vector2,
@@ -22,3 +27,11 @@ func calculate_move_velocity(
 	if direction.y == -1.0:
 		new_velocity.y = speed.y * direction.y
 	return new_velocity
+	
+	
+func _process(delta):
+	if Input.is_action_pressed("move_right"):
+		$AnimationPlayer.play("move")
+	else:
+		$AnimationPlayer.play("idle")
+
