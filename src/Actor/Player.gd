401e09858b23655cca9d6820f8877fefad5ab79f
Arvi-beep-boop <79840246+Arvi-beep-boop@users.noreply.github.com>
2021-03-06 23:13:20 +0100

    project settings and movement tweaking

--- src/Actor/Player.gd
+++ src/Actor/Player.gd
@@ -10,11 +10,15 @@ func _physics_process(delta: float) -> void:
 	
 	
 func get_direction() -> Vector2:
-	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
-	)
+	var direction = Vector2()
+	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		direction.y = -1.0
+	else:
+		direction.y = 0.0
+	
+	return direction
 	
 func calculate_move_velocity(
 	linear_velocity: Vector2,
@@ -25,7 +29,7 @@ func calculate_move_velocity(
 	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
 	
-	if direction.y == -1.0:
+	if direction.y <= -1.0:
 		new_velocity.y = speed.y * direction.y
 	
 	return new_velocity
@@ -34,11 +38,11 @@ func calculate_move_velocity(
 func _process(delta):
 	if Input.is_action_pressed("move_right"):
 		$SLIME_FRAMES_ROW.flip_h = false
-		$AnimationPlayer.play("moveRight")
+		$AnimationPlayer.play("move")
 		
 	elif Input.is_action_pressed("move_left"):
 		$SLIME_FRAMES_ROW.flip_h = true
-		$AnimationPlayer.play("moveLeft")
+		$AnimationPlayer.play("move")
 		
 	else:
 		$AnimationPlayer.play("idle")
