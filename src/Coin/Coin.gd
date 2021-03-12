5f4f59402a2c8dd7da16a0346f6638765910b3b7
Arvi-beep-boop <79840246+Arvi-beep-boop@users.noreply.github.com>
2021-03-13 00:46:59 +0100

    New changes
    lol pog gl hf

--- src/Coin/Coin.gd
+++ src/Coin/Coin.gd
@@ -1,5 +1,8 @@
 extends Area2D
 
+
+export var score: = 100
+
 onready var animationPlayer = $AnimationPlayer
 func _ready():
 	coin_animation()
@@ -7,6 +10,6 @@ func coin_animation():
 	$AnimationPlayer.play("Bounce")
 
 
-
 func _on_body_entered(body):
+	PlayerData.score += score
 	animationPlayer.play("fade_out")
