bca1555cf3afdfe41f08b66ad2f24d0c08ea8826
Arvi-beep-boop <79840246+Arvi-beep-boop@users.noreply.github.com>
2021-03-16 11:57:29 +0100

    Bug repaired
    Game is no-longer crashed when exiting to main menu.

--- src/UserInterface/UserInterface.gd
+++ src/UserInterface/UserInterface.gd
@@ -4,16 +4,20 @@ onready var scene_tree: = get_tree()
 onready var pause_overlay: ColorRect = get_node("PauseOverlay")
 onready var score: Label = get_node("Score")
 
-var paused: = false setget set_paused
+export var paused: = false setget set_paused
+
+
 
 func _ready() -> void:
 	PlayerData.connect("score_updated", self, "update_interface")
 	update_interface()
+	
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Pause"):
 		self.paused = not paused
 		scene_tree.set_input_as_handled()
+
 
 func update_interface() -> void:
 	score.text = "Score: %s" %PlayerData.score
@@ -22,3 +26,5 @@ func set_paused(value: bool) -> void:
 	paused = value
 	scene_tree.paused = value
 	pause_overlay.visible = value
+
+
